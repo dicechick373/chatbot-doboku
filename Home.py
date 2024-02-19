32abedad9386ffafe34964d311a2d88e29bb691d
dicechick373 <dicechick373@gmail.com>
2024-02-19 16:24:09 +0900

    test

--- Home.py
+++ Home.py
@@ -7,6 +7,30 @@ from streamlit_chat import message
 # os.environ["http_proxy"] = st.secrets["PROXY"]
 # os.environ["https_proxy"] = st.secrets["PROXY"]
 
+def vectorstore_dir(stock):
+    if stock == '土木工事共通仕様書':
+        return "vectorstore/faiss/kyoutsuu_shiyousyo"
+    elif stock == '土木請負工事必携':
+        return "vectorstore/faiss/hikkei"
+    elif stock == '土木技術管理規程集【道路Ⅰ編】':
+        return "vectorstore/faiss/kiteisyuu/douro1"
+    elif stock == '土木技術管理規程集【道路Ⅱ編】':
+        return "vectorstore/faiss/kiteisyuu/douro2"
+    elif stock == '土木技術管理規程集【河川編】':
+        return "vectorstore/faiss/kiteisyuu/kasen"
+    elif stock == '土木技術管理規程集【砂防編_砂防】':
+        return "vectorstore/faiss/kiteisyuu/sabou"
+    elif stock == '土木技術管理規程集【砂防編_急傾斜】':
+        return "vectorstore/faiss/kiteisyuu/kyuukeisya"
+    elif stock == '土木技術管理規程集【砂防編_地すべり】 ':
+        return "vectorstore/faiss/kiteisyuu/jisuberi"
+    elif stock == '近畿地整設計便覧【土木工事共通編】':
+        return "vectorstore/faiss/chiseibinran/kyoutsuu"
+    elif stock == '近畿地整設計便覧【道路編】 ':
+        return "vectorstore/faiss/chiseibinran/douro"
+    elif stock == '近畿地整設計便覧【河川編】 ':
+        return "vectorstore/faiss/chiseibinran/kasen"
+    
 # sidebar
 with st.sidebar:
     openai_api_key = st.sidebar.text_input('OpenAI API Key')
